c2b0932ba5e7f62cb58ddaa3207a64227b30e0a4
SEIU iMac 5 <imac5@SEIUs-iMac-5.local>
2026-09-14 15:03:06 +0700

    Add lab 1

---
+++ lab 1/README.md
@@ -0,0 +1,3 @@
+# GEO
+# hello
+# hello
---
+++ lab 1/hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by SEIU iMac 5 on 14/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
